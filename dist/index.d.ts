e0e37b99e8725dfc1ebd71eaed5c5cc5c6c9f913
0xf965 <40121100+0xf965@users.noreply.github.com>
2025-12-28 10:30:33 +0100

    added dist.

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+export { default as Forum } from './components/Forum.svelte';
+export * from './ergo/commentFetch';
+export * from './ergo/commentObject';
+export * from './ergo/commentStore';
+export * from './ergo/envs';
+export * from './ergo/object';
+export * from './ergo/store';
+export * from './ergo/submit';
+export * from './ergo/utils';
